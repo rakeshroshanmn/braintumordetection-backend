c2045c1a31220de2383cdc78db49c553eb55f396
Rakesh Roshan <rakeshroshan4411@gmail.com>
2021-09-02 15:18:00 +0530

    cmt

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
 const express = require("express");
-const AWS = require("aws-sdk");
+// const AWS = require("aws-sdk");
 const cors = require("cors");
 const app = express();
 const port = process.env.port || 3000;
 app.use(express.json());
 app.use(cors());
-app.post("/", function (req, res) {
-  var rekognition = new AWS.Rekognition({
-    region: "ap-south-1",
-    accessKeyId: "AKIAX4MFRFGGNCBMO5FC",
-    secretAccessKey: "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",
-  });
-  var params = {
-    Image: {
-      S3Object: {
-        Bucket: req.body.bucket,
-        Name: req.body.key,
-      },
-    },
-    ProjectVersionArn:
-      "arn:aws:rekognition:ap-south-1:541982796172:project/BraintumorDetection/version/BraintumorDetection.2021-09-02T14.27.07/1630573027056" /* required */,
-  };
+// app.post("/", function (req, res) {
+//   var rekognition = new AWS.Rekognition({
+//     region: "ap-south-1",
+//     accessKeyId: "AKIAX4MFRFGGNCBMO5FC",
+//     secretAccessKey: "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",
+//   });
+//   var params = {
+//     Image: {
+//       S3Object: {
+//         Bucket: req.body.bucket,
+//         Name: req.body.key,
+//       },
+//     },
+//     ProjectVersionArn:
+//       "arn:aws:rekognition:ap-south-1:541982796172:project/BraintumorDetection/version/BraintumorDetection.2021-09-02T14.27.07/1630573027056" /* required */,
+//   };
 
-  rekognition.detectCustomLabels(params, function (err, data) {
-    if (err) console.log(err, err.stack);
-    else res.send(data);
-  });
-});
+//   rekognition.detectCustomLabels(params, function (err, data) {
+//     if (err) console.log(err, err.stack);
+//     else res.send(data);
+//   });
+// });
 app.get("/", function (req, res) {
   res.send("Hello Everyone");
 });
